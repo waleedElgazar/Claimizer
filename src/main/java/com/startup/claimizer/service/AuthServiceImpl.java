07ee55fbe7065360dca7e2b71e1d96115085916a
waleed reda <waleed.reda@fawry.com>
2023-10-05 14:22:14 +0300

    start using logs

--- src/main/java/com/startup/claimizer/service/AuthServiceImpl.java
+++ src/main/java/com/startup/claimizer/service/AuthServiceImpl.java
@@ -12,6 +12,8 @@ import com.waleedreda.core.common.AppResponseUtil;
 import com.waleedreda.core.common.ErrorCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
@@ -29,27 +31,34 @@ public class AuthServiceImpl implements AuthService {
     @Autowired
     UserSpecificationBuilder userSpecificationBuilder;
 
+    Logger logger = LogManager.getLogger(AuthServiceImpl.class);
+
     @Override
     public AppResponse<UserDto> loginUser(UserDto userDetails) {
+        logger.info("Login Request the request body is ", userDetails.toString());
         try {
             UserCriteria userCriteria = new UserCriteria();
             userCriteria.setEmail(userDetails.getEmail());
             Specification<UserEntity> searchSpecification = getUserSpecificationBuilder().getSearchSpecification(userCriteria);
             List<UserEntity> userEntityList = getUserRepo().findAll(searchSpecification);
             if (userEntityList.isEmpty()) {
+                logger.error("Login Request the Response body is This user with the Criteria " + userDetails.toString() + " isn't found");
                 return AppResponseUtil.buildFailedResponse(ErrorCode.NOT_FOUND, "This user with the Criteria " + userDetails.toString() + " isn't found");
             }
             UserDto userDto = getUserMapper().ConvertToDto(userEntityList.get(0));
-            String password = CommonService.decrypt(userDetails.getPassword());
+            String password = CommonService.encrypt(userDetails.getPassword());
             if (userDto.getPassword().equals(password)) {
                 userDto.setPassword(userDetails.getPassword());
-                return AppResponseUtil.buildSuccessResponse(userDto);
+                AppResponse<UserDto> userDtoAppResponse = AppResponseUtil.buildSuccessResponse(userDto);
+                logger.info("Login Request the Response body is " + userDtoAppResponse.toString());
+                return userDtoAppResponse;
             } else {
+                logger.error("Login Response the Response body is " + ErrorCode.INVALID_DATA.toString());
                 return AppResponseUtil.buildFailedResponse(ErrorCode.INVALID_DATA, "Invalid Password");
             }
         } catch (Exception exception) {
-            // TODO need to change the error code
-            return AppResponseUtil.buildFailedResponse(ErrorCode.NOT_FOUND, "Error happen while during the request the error msg is " + exception.getMessage());
+            logger.error("Login Response the Response body is " + ErrorCode.GENERAL.toString());
+            return AppResponseUtil.buildFailedResponse(ErrorCode.GENERAL, "Error happen while during the request the error msg is " + exception.getMessage());
         }
     }
 
