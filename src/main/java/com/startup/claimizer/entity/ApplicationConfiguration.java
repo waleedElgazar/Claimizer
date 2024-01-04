66ba5815aee614d0a5c8f28d92f6db0e908a149b
waleed reda <waleed.reda@fawry.com>
2024-01-04 11:44:35 +0200

    update liquibase configuration

---
+++ src/main/java/com/startup/claimizer/entity/ApplicationConfiguration.java
@@ -0,0 +1,29 @@
+package com.startup.claimizer.entity;
+
+import com.waleedreda.core.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "APPLICATION_CONFIGURATION")
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApplicationConfiguration extends BaseEntity {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "S_APPLICATION_CONFIGURATION")
+    @SequenceGenerator(name = "S_APPLICATION_CONFIGURATION", sequenceName = "S_APPLICATION_CONFIGURATION", allocationSize = 1)
+    private Long id;
+//
+    @Column(name = "PARAMETER_KEY", unique = true, nullable = false)
+    private String parameterKey;
+
+
+    @Column(name = "PARAMETER_VALUE", nullable = false)
+    private String parameterValue;
+}
