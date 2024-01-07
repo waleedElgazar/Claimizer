2b6bf6bafa738a0cb127feac8e7f42abb73cbcbb
waleed reda <38845105+waleedElgazar@users.noreply.github.com>
2024-01-07 02:20:22 +0200

    update application configuration entity table

--- src/main/java/com/startup/claimizer/entity/ApplicationConfigurationEntity.java
+++ src/main/java/com/startup/claimizer/entity/ApplicationConfigurationEntity.java
@@ -10,16 +10,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "APPLICATION_CONFIGURATION")
+@Table(name = "APPLICATION_CONFIGURATION_ENTITY")
 @AllArgsConstructor
 @NoArgsConstructor
-public class ApplicationConfiguration extends BaseEntity {
+public class ApplicationConfigurationEntity extends BaseEntity {
     @Id
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "S_APPLICATION_CONFIGURATION")
     @SequenceGenerator(name = "S_APPLICATION_CONFIGURATION", sequenceName = "S_APPLICATION_CONFIGURATION", allocationSize = 1)
     private Long id;
-//
+
+    @Column(name = "GROUP_NAME", nullable = false)
+    private String groupName;
+
+
     @Column(name = "PARAMETER_KEY", unique = true, nullable = false)
     private String parameterKey;
 
